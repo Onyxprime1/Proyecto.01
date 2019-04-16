c6b151e645efe314ca08014bb3989cf139677b5c
yisus <kevinpinedagots@gmail.com>
2019-04-15 21:45:33 -0600

    ok.01

--- app/src/main/java/kevin/com/proyecto01/adaptadores/adaptador.java
+++ app/src/main/java/kevin/com/proyecto01/adaptadores/adaptador.java
@@ -1,21 +1,17 @@
 package kevin.com.proyecto01.adaptadores;
 
 import android.app.Activity;
-import android.app.ActivityOptions;
-import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.widget.RecyclerView;
-import android.transition.Explode;
 import android.view.LayoutInflater;
 import android.view.View;
+
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
@@ -60,16 +56,6 @@ public class adaptador extends RecyclerView.Adapter<adaptador.ViewHolder> {
                 intent.putExtra("like",lista.get(i).getLike());
                 intent.putExtra("img",lista.get(i).getImagen());
                 context.startActivity(intent);
-               /* context.startActivity(intent);
-              /*  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
-                    Explode explode = new Explode();
-                    explode.setDuration(1000);
-                    context.getWindow().setExitTransition(explode);
-                    context.startActivity(intent,ActivityOptionsCompat.makeSceneTransitionAnimation(
-                            context,v,context.getString(R.string.tramsiton)).toBundle());
-                }else {
-                    context.startActivity(intent);
-                }*/
             }
         });
     }
@@ -93,7 +79,6 @@ public class adaptador extends RecyclerView.Adapter<adaptador.ViewHolder> {
             texto2 = itemView.findViewById(R.id.timecard);
             texto3 = itemView.findViewById(R.id.likeNumber);
         }
-
-
     }
+
 }
