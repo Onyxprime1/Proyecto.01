8e9ae573634a0f46312df32ec621d352d6e71832
angelcatu <angel.manuel.elias.gt@gmail.com>
2019-05-24 14:49:12 -0600

    Revert "Merge remote-tracking branch 'origin/segundo' into segundo"
    This reverts commit 213e56fe9a163ef7c14b86e7dfebfe05c753d2b7, reversing
    changes made to 182a692dac6b5c426368453791f8991195be72dc.

--- app/src/main/java/kevin/com/proyecto01/view/fragment/Fragment3.java
+++ app/src/main/java/kevin/com/proyecto01/view/fragment/Fragment3.java
@@ -7,16 +7,14 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
 
 import kevin.com.proyecto01.R;
-import kevin.com.proyecto01.adaptadores.adaptador;
-import kevin.com.proyecto01.adaptadores.modelo;
+import kevin.com.proyecto01.adaptadores.Adaptador;
+import kevin.com.proyecto01.Database.Entidad.ProgramerEntity;
 
 /**
  *
@@ -25,7 +23,7 @@ import kevin.com.proyecto01.adaptadores.modelo;
 public class Fragment3 extends Fragment {
 
     RecyclerView recy3;
-    adaptador adapt;
+    Adaptador adapt;
     GridLayoutManager gridLayoutManager;
 
     public Fragment3() {
@@ -41,20 +39,20 @@ public class Fragment3 extends Fragment {
        gridLayoutManager = new GridLayoutManager(view.getContext(),2);
         recy3.setHasFixedSize(true);
         recy3.setLayoutManager(gridLayoutManager);
-        adapt = new adaptador(listade(),R.layout.card_picture,getActivity());
+        adapt = new Adaptador(listade(),R.layout.card_picture,getActivity());
         recy3.setAdapter(adapt);
         return view;
     }
 
-    public ArrayList<modelo> listade(){
-        ArrayList<modelo>listo = new ArrayList<>();
-        listo.add(new modelo("http://conrumboaningunaparte.com/wp-content/uploads/2017/07/leon-blanco-y-negro-183361-2.jpg","kevin","infinito","10000"));
-        listo.add(new modelo("http://s1.1zoom.me/big0/512/Lions_462377.jpg","kevin","infinito","10000"));
-        listo.add(new modelo("https://us.123rf.com/450wm/huettenhoelscher/huettenhoelscher1606/huettenhoelscher160600268/60807044-le%C3%B3n-en-blanco-y-negro.jpg?ver=6","kevin","infinito","10000"));
-        listo.add(new modelo("https://ae01.alicdn.com/kf/HTB1OSOeNFXXXXbsXFXXq6xXFXXXD/Env-o-Gratuito-de-Una-Pieza-de-Papel-Tapiz-de-Le-n-Real-Gato-Salvaje-Caliente.jpg","kevin","infinito","10000"));
-        listo.add(new modelo("https://www.dhresource.com/0x0s/f2-albu-g4-M01-E3-B5-rBVaEFoTsMKAL_kXAAD5kUIds5c887.jpg/cartel-de-le-n-blanco-y-negro-decoraci-n.jpg","kevin","infinito","10000"));
-        listo.add(new modelo("https://desenio.es/bilder/artiklar/zoom/2909_1.jpg","kevin","infinito","10000"));
-        listo.add(new modelo("https://http2.mlstatic.com/cuadro-moderno-leon-blanco-y-negro-5-piezas-114x185cm-D_NQ_NP_820304-MLM25894387403_082017-F.jpg","kevin","infinito","10000"));
+    public ArrayList<ProgramerEntity> listade(){
+        ArrayList<ProgramerEntity>listo = new ArrayList<>();
+        listo.add(new ProgramerEntity("http://conrumboaningunaparte.com/wp-content/uploads/2017/07/leon-blanco-y-negro-183361-2.jpg","kevin","infinito","10000", true));
+        listo.add(new ProgramerEntity("http://s1.1zoom.me/big0/512/Lions_462377.jpg","kevin","infinito","10000", false));
+        listo.add(new ProgramerEntity("https://us.123rf.com/450wm/huettenhoelscher/huettenhoelscher1606/huettenhoelscher160600268/60807044-le%C3%B3n-en-blanco-y-negro.jpg?ver=6","kevin","infinito","10000", false));
+        listo.add(new ProgramerEntity("https://ae01.alicdn.com/kf/HTB1OSOeNFXXXXbsXFXXq6xXFXXXD/Env-o-Gratuito-de-Una-Pieza-de-Papel-Tapiz-de-Le-n-Real-Gato-Salvaje-Caliente.jpg","kevin","infinito","10000", false));
+        listo.add(new ProgramerEntity("https://www.dhresource.com/0x0s/f2-albu-g4-M01-E3-B5-rBVaEFoTsMKAL_kXAAD5kUIds5c887.jpg/cartel-de-le-n-blanco-y-negro-decoraci-n.jpg","kevin","infinito","10000", false   ));
+        listo.add(new ProgramerEntity("https://desenio.es/bilder/artiklar/zoom/2909_1.jpg","kevin","infinito","10000", true));
+        listo.add(new ProgramerEntity("https://http2.mlstatic.com/cuadro-moderno-leon-blanco-y-negro-5-piezas-114x185cm-D_NQ_NP_820304-MLM25894387403_082017-F.jpg","kevin","infinito","10000", true));
 
         return listo;
     }
