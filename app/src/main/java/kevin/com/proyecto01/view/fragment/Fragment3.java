396f23f68d4d7e730a0a0c4420e1944600d59daf
Jimena-chan <jiimenagramajo@gmail.com>
2019-05-23 22:19:25 -0600

    jime

--- app/src/main/java/kevin/com/proyecto01/view/fragment/Fragment3.java
+++ app/src/main/java/kevin/com/proyecto01/view/fragment/Fragment3.java
@@ -19,7 +19,6 @@ import kevin.com.proyecto01.adaptadores.adaptador;
 import kevin.com.proyecto01.adaptadores.modelo;
 
 /**
- *
  * A simple {@link Fragment} subclass.
  */
 public class Fragment3 extends Fragment {
@@ -34,27 +33,27 @@ public class Fragment3 extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_fragment3, container, false);
         recy3 = view.findViewById(R.id.recy3);
-       gridLayoutManager = new GridLayoutManager(view.getContext(),2);
+        gridLayoutManager = new GridLayoutManager(view.getContext(), 2);
         recy3.setHasFixedSize(true);
         recy3.setLayoutManager(gridLayoutManager);
-        adapt = new adaptador(listade(),R.layout.card_picture,getActivity());
+        adapt = new adaptador(listade(), R.layout.card_picture, getActivity());
         recy3.setAdapter(adapt);
         return view;
     }
 
-    public ArrayList<modelo> listade(){
-        ArrayList<modelo>listo = new ArrayList<>();
-        listo.add(new modelo("http://conrumboaningunaparte.com/wp-content/uploads/2017/07/leon-blanco-y-negro-183361-2.jpg","kevin","infinito","10000"));
-        listo.add(new modelo("http://s1.1zoom.me/big0/512/Lions_462377.jpg","kevin","infinito","10000"));
-        listo.add(new modelo("https://us.123rf.com/450wm/huettenhoelscher/huettenhoelscher1606/huettenhoelscher160600268/60807044-le%C3%B3n-en-blanco-y-negro.jpg?ver=6","kevin","infinito","10000"));
-        listo.add(new modelo("https://ae01.alicdn.com/kf/HTB1OSOeNFXXXXbsXFXXq6xXFXXXD/Env-o-Gratuito-de-Una-Pieza-de-Papel-Tapiz-de-Le-n-Real-Gato-Salvaje-Caliente.jpg","kevin","infinito","10000"));
-        listo.add(new modelo("https://www.dhresource.com/0x0s/f2-albu-g4-M01-E3-B5-rBVaEFoTsMKAL_kXAAD5kUIds5c887.jpg/cartel-de-le-n-blanco-y-negro-decoraci-n.jpg","kevin","infinito","10000"));
-        listo.add(new modelo("https://desenio.es/bilder/artiklar/zoom/2909_1.jpg","kevin","infinito","10000"));
-        listo.add(new modelo("https://http2.mlstatic.com/cuadro-moderno-leon-blanco-y-negro-5-piezas-114x185cm-D_NQ_NP_820304-MLM25894387403_082017-F.jpg","kevin","infinito","10000"));
+    public ArrayList<modelo> listade() {
+        ArrayList<modelo> listo = new ArrayList<>();
+        listo.add(new modelo("http://conrumboaningunaparte.com/wp-content/uploads/2017/07/leon-blanco-y-negro-183361-2.jpg", "kevin", "infinito", "10000"));
+        listo.add(new modelo("http://s1.1zoom.me/big0/512/Lions_462377.jpg", "kevin", "infinito", "10000"));
+        listo.add(new modelo("https://us.123rf.com/450wm/huettenhoelscher/huettenhoelscher1606/huettenhoelscher160600268/60807044-le%C3%B3n-en-blanco-y-negro.jpg?ver=6", "kevin", "infinito", "10000"));
+        listo.add(new modelo("https://ae01.alicdn.com/kf/HTB1OSOeNFXXXXbsXFXXq6xXFXXXD/Env-o-Gratuito-de-Una-Pieza-de-Papel-Tapiz-de-Le-n-Real-Gato-Salvaje-Caliente.jpg", "kevin", "infinito", "10000"));
+        listo.add(new modelo("https://www.dhresource.com/0x0s/f2-albu-g4-M01-E3-B5-rBVaEFoTsMKAL_kXAAD5kUIds5c887.jpg/cartel-de-le-n-blanco-y-negro-decoraci-n.jpg", "kevin", "infinito", "10000"));
+        listo.add(new modelo("https://desenio.es/bilder/artiklar/zoom/2909_1.jpg", "kevin", "infinito", "10000"));
+        listo.add(new modelo("https://http2.mlstatic.com/cuadro-moderno-leon-blanco-y-negro-5-piezas-114x185cm-D_NQ_NP_820304-MLM25894387403_082017-F.jpg", "kevin", "infinito", "10000"));
 
         return listo;
     }
--- app/src/main/java/kevin/com/proyecto01/view/fragment/Fragment4.java
+++ app/src/main/java/kevin/com/proyecto01/view/fragment/Fragment4.java
@@ -2,6 +2,7 @@ package kevin.com.proyecto01.view.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,65 +11,101 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import kevin.com.proyecto01.R;
 import kevin.com.proyecto01.adaptadores.AdaptadorChats;
 import kevin.com.proyecto01.modelos.ChatsModel;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class Fragment4 extends Fragment {
 
     RecyclerView mRecyclerView;
     AdaptadorChats mAdaptadorChats;
     ArrayList<ChatsModel> mListaChats;
     Toolbar mToolbar;
+
+    CircleImageView fotoPerfil;
+    TextView username;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+
     public Fragment4() {
         // Required empty public constructor
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_fragment4, container, false);
+        gui(view);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    username.setText(user.getDisplayName());
+                    Glide.with(getContext()).load(user.getPhotoUrl()).into(fotoPerfil);
+                }
+            }
+        };
+        setHasOptionsMenu(true);
+        showToolbar();
+        mListaChats = new ArrayList<>();
+        cargarListaChats();
+        initRecycler();
+        return view;
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-       View view = inflater.inflate(R.layout.fragment_fragment4, container, false);
-       //showToolbar("Chats",view);
-       mRecyclerView = view.findViewById(R.id.recycler_chats);
-       mListaChats = new ArrayList<>();
-       cargarListaChats();
-       initRecycler();
-       return view;
+    public void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(firebaseAuthListener);
     }
 
-    public void cargarListaChats(){
-        mListaChats.add(new ChatsModel(" ","Carlos","Hola","28/03/2018",2));
-        mListaChats.add(new ChatsModel(" ","Carlos","Hola","14/03/2019",2));
-        mListaChats.add(new ChatsModel(" ","Carlos","Hola","28/01/2019",2));
-        mListaChats.add(new ChatsModel(" ","Carlos","Hola","28/03/2017",2));
-        mListaChats.add(new ChatsModel(" ","Carlos","Hola","28/03/2017",2));
-        mListaChats.add(new ChatsModel(" ","Carlos","Hola","28/03/2017",2));
-        mListaChats.add(new ChatsModel(" ","Carlos","Hola","28/03/2017",2));
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (firebaseAuthListener != null) {
+            firebaseAuth.removeAuthStateListener(firebaseAuthListener);
+        }
     }
 
-    public void initRecycler(){
+    public void showToolbar() {
+        AppCompatActivity activi = (AppCompatActivity) getActivity();
+        activi.setSupportActionBar(mToolbar);
+        activi.getSupportActionBar().setTitle("");
+        activi.getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+    }
+
+    public void gui(View view) {
+        mRecyclerView = view.findViewById(R.id.recycler_chats);
+        fotoPerfil = view.findViewById(R.id.fperfil);
+        username = view.findViewById(R.id.username);
+        mToolbar = view.findViewById(R.id.toolbar_chat);
+    }
+
+
+    public void cargarListaChats() {
+        mListaChats.add(new ChatsModel(" ", "Carlos", "Hola", "28/03/2018", 2));
+        mListaChats.add(new ChatsModel(" ", "Carlos", "Hola", "14/03/2019", 2));
+        mListaChats.add(new ChatsModel(" ", "Carlos", "Hola", "28/01/2019", 2));
+        mListaChats.add(new ChatsModel(" ", "Carlos", "Hola", "28/03/2017", 2));
+    }
+
+    public void initRecycler() {
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         mRecyclerView.setHasFixedSize(true);
-        mAdaptadorChats = new AdaptadorChats(mListaChats,getContext());
+        mAdaptadorChats = new AdaptadorChats(mListaChats, getContext());
         mRecyclerView.setAdapter(mAdaptadorChats);
-
-    }
-    public void showToolbar(String tittle,View view) {
-
-        AppCompatActivity activi = (AppCompatActivity) getActivity();
-        mToolbar = view.findViewById(R.id.toolbars);
-        activi.setSupportActionBar(mToolbar);
-        activi.getSupportActionBar().setTitle(tittle);
-        activi.getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-
     }
 
 }
