6788c99137d5572aeea43ccbd7aee401f385ba45
Carlos-vasquez <cv220699@gmail.com>
2019-05-28 14:34:28 -0600

    Tabs del perfil

---
+++ app/src/main/java/kevin/com/proyecto01/tabsPerfil/Historial.java
@@ -0,0 +1,30 @@
+package kevin.com.proyecto01.tabsPerfil;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kevin.com.proyecto01.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Historial extends Fragment {
+
+
+    public Historial() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_historial, container, false);
+    }
+
+}
